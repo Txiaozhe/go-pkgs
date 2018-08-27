d9a52d6a0a493ac73d878c22a05e2cf072542801
TangXiaoji <txiaozhe@gmail.com>
2018-08-27 08:44:25 +0800

    snowflake

---
+++ snowflake/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bwmarrin/snowflake"
+)
+
+func main() {
+	n, err := snowflake.NewNode(1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for i := 0; i < 3; i++ {
+		id := n.Generate()
+		fmt.Println("id: ", id)
+		fmt.Println("node: ", id.Node(), " step: ", id.Step(), " time: ", id.Time())
+	}
+}
