00ffb7f8f3dcaf4fffe75bd3ec790084930eed3a
TangXiaoji <txiaozhe@gmail.com>
2018-09-12 09:05:57 +0800

    array

--- array/main/mian.go
+++ array/main/mian.go
@@ -5,11 +5,19 @@ import (
 )
 
 var arr1 = [8]string{"go", "python", "java", "c", "c++", "php"}
-var arr2 = [8]string{0: "go", 1: "python", 2: "java", 3: "c", 4: "c++", 5: "php"}
+var arr2 = [8]string{6: "go", 1: "python", 2: "java", 3: "c", 4: "c++", 5: "php"}
 var arr3 = [...]string{"go", "python", "java", "c", "c++", "php"}
 
 func main() {
 	fmt.Println(len(arr1))
 	fmt.Println(len(arr2))
 	fmt.Println(len(arr3))
+
+	for idx, ele := range arr1 {
+		fmt.Println("idx: ", idx, "ele: ", ele)
+	}
+
+	for idx, ele := range arr2 {
+		fmt.Println("idx: ", idx, "ele: ", ele)
+	}
 }
