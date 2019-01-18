a8ca1e3815dc19f0e97da8ac546a9ff3e236d05e
Txiaozhe <txiaozhe@gmail.com>
2019-01-18 11:49:48 +0800

    Update root.go

--- cobra/root/root.go
+++ cobra/root/root.go
@@ -8,17 +8,19 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
+	Use:   "hugo [command] (flags)",
 	Short: "Hugo is a very fast static site generator",
 	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
+ love by spf13 and friends in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(cmd)
+		fmt.Println("hugo run...")
 	},
 }
 
 func Execute() {
+	path := rootCmd.CommandPath()
+
+	fmt.Printf("command path: %s\n", path)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
