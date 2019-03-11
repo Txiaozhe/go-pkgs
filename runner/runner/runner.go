eaf239471806b9050b96aaca8225261270931820
Txiaozhe <txiaozhe@gmail.com>
2019-03-11 13:36:14 +0800

    fix something

--- runner/runner/runner.go
+++ runner/runner/runner.go
@@ -3,50 +3,59 @@ package runner
 import (
 	"errors"
 	"os"
-	"os/Signal"
+	"os/signal"
 	"time"
 )
 
+// Runner runs a set of tasks within a given timeout and can be
+// shut down on an operating system interrupt.
 type Runner struct {
 	interrupt chan os.Signal
 	complete  chan error
-	timeout <  - chan time.Time
-	tasks      []func()int
+	timeout   <-chan time.Time
+	tasks     []func(int)
 }
 
+// ErrTimeout is returned when a value is received on the timeout channel.
 var ErrTimeout = errors.New("received timeout")
 
+// ErrInterrupt is returned when an event from the OS is received.
 var ErrInterrupt = errors.New("received interrupt")
 
-func New(d time.Duration) * Runner {
-	return & Runner {
-		interrupt:make(chan os.Signal, 1)
-		complete:make(chan error)
-		timeout:time.After(d)
+// New returns a new ready-to-use Runner.
+func New(d time.Duration) *Runner {
+	return &Runner{
+		interrupt: make(chan os.Signal, 1),
+		complete:  make(chan error),
+		timeout:   time.After(d),
 	}
 }
 
-func (r * Runner)Add(tasks ...func(int)) {
+// Add attaches tasks to the Runner. A task is a function that
+// takes an int ID.
+func (r *Runner) Add(tasks ...func(int)) {
 	r.tasks = append(r.tasks, tasks...)
 }
 
-func (r * Runner)Start()error {
+// Start runs all tasks and monitors channel events.
+func (r *Runner) Start() error {
 	signal.Notify(r.interrupt, os.Interrupt)
 
 	go func() {
-		r.complete <  - r.run()
+		r.complete <- r.run()
 	}()
 
 	select {
-	case err: =  <  - r.complete:
+	case err := <-r.complete:
 		return err
-	case r.timeout:
+	case <-r.timeout:
 		return ErrTimeout
 	}
 }
 
-func (r * Runner)run()error {
-	for id, task: = range r.tasks {
+// run executes each registered task.
+func (r *Runner) run() error {
+	for id, task := range r.tasks {
 		if r.gotInterrupt() {
 			return ErrInterrupt
 		}
@@ -57,9 +66,10 @@ func (r * Runner)run()error {
 	return nil
 }
 
-func (r * Runner)gotInterrupt()bool {
+// gotInterrupt verifies if the interrupt signal has been issued.
+func (r *Runner) gotInterrupt() bool {
 	select {
-	case <  - r.interrupt:
+	case <-r.interrupt:
 		signal.Stop(r.interrupt)
 		return true
 	default:
